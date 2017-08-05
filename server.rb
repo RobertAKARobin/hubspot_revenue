b89b04045651dadb2e9b597f10a1b30dfe3a772a
Robin Thomas <>
2017-08-04 21:45:23 -0500

    Upload and parse CSV to hash

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/json"
+require "csv"
 
 get "/" do
 	redirect "/index.html"
@@ -8,4 +9,18 @@ end
 
 get "/api" do
 	json({success: true, message: "This is from the API"})
+end
+
+post "/upload" do
+	data = []
+	csv = CSV.parse(File.open(params[:file][:tempfile], "r").read)
+	fields = csv.shift
+	fields = fields.map do |field|
+		field.downcase.gsub(/ /, "_").gsub(/[^a-z0-9_]/, "")
+	end
+
+	csv[0..9].each do |record|
+		data.push Hash[fields.zip(record)]
+	end
+	data.to_json
 end
