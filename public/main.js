2d38ad5618610a4d6cdf9f5f3fc6466ed4344887
Robin Thomas <>
2017-08-29 08:47:14 -0500

    Put refresh socket in closure

--- public/main.js
+++ public/main.js
@@ -2,50 +2,37 @@
 
 (function(){
 
-	var Component = (function(){
+	var LoadingButton = (function(){
 		var message;
 
-		var socket = Socket.new();
-		socket.onstart = function(){
-			models.loading.inProgress = true;
-			models.loading.progress = '0';
-			m.redraw();
-		}
-		socket.onmessage = function(response){
-			models.loading.progress = Math.round((response.offset * 100) / response.total);
-			m.redraw();
-		}
-		socket.onerror = function(error){
-			models.loading.progress = false;
-			models.loading.error = error.message;
-			m.redraw();
-		}
-		socket.onclose = function(){
-			models.loading.inProgress = false;
-			m.redraw();
+		var sockets = {
+			refresh: (function(){
+				var socket = Socket.new();
+				socket.onstart = function(){
+					models.loading.inProgress = true;
+					models.loading.progress = '0';
+					m.redraw();
+				}
+				socket.onmessage = function(response){
+					models.loading.progress = Math.round((response.offset * 100) / response.total);
+					m.redraw();
+				}
+				socket.onerror = function(error){
+					models.loading.progress = false;
+					models.loading.error = error.message;
+					m.redraw();
+				}
+				socket.onclose = function(){
+					models.loading.inProgress = false;
+					m.redraw();
+				}
+				return socket
+			}())
 		}
 
 		var events = {};
 		events.refresh = function(){
-			socket.send('/refresh');
-		}
-
-		var views = {};
-		views.loadingButton = function(){
-			var attrs = {};
-			var progress = (models.loading.progress ? models.loading.progress + '%' : '');
-			var message = (models.loading.inProgress ? 'Loading...' : (models.loading.error || ''));
-
-			if(models.loading.inProgress){
-				attrs.onclick = null;
-			}else{
-				attrs.href = '#';
-				attrs.onclick = events.refresh;
-			}
-			return m('p', [
-				m('a', attrs, 'Refresh'),
-				m('span', (' ' + progress + ' ' + message))
-			]);
+			sockets.refresh.send('/refresh');
 		}
 
 		var models = {};
@@ -68,15 +55,25 @@
 				});
 			},
 			view: function(){
-				return [
-					m('h1', 'The API says: ' + message),
-					views.loadingButton()
-				]
+				var attrs = {};
+				var progress = (models.loading.progress ? models.loading.progress + '%' : '');
+				var message = (models.loading.inProgress ? 'Loading...' : (models.loading.error || ''));
+	
+				if(models.loading.inProgress){
+					attrs.onclick = null;
+				}else{
+					attrs.href = '#';
+					attrs.onclick = events.refresh;
+				}
+				return m('p', [
+					m('a', attrs, 'Refresh'),
+					m('span', (' ' + progress + ' ' + message))
+				]);
 			}
 		}
 	})();
 
 	document.addEventListener('DOMContentLoaded', function(){
-		m.mount(document.getElementById('display'), Component);
+		m.mount(document.getElementById('loading'), LoadingButton);
 	});
 })();
